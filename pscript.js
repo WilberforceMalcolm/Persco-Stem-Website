924e874d1863534a02772c9b36348c462cc02713
WilberforceMalcolm <wilberforcemalcolm14@gmail.com>
2025-03-30 23:09:21 +0000

    api

--- pscript.js
+++ pscript.js
@@ -129,7 +129,8 @@ slideInBtn.addEventListener('click', () => {
 
 
 function fetchBlogPosts() {
-  fetch('https://newsapi.org/v2/top-headlines?category=technology&apiKey=e854aba1e9944bbf8793220962bbfdbb')
+  const apiKey = 'e854aba1e9944bbf8793220962bbfdbb';
+  fetch(`https://newsapi.org/v2/top-headlines?category=technology&apiKey=${apiKey}`)
     .then(response => response.json())
     .then(data => {
       let articles = data.articles;
