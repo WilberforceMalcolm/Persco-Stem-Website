df02e9ab61ce0dd06badadafa7fd5c1c46000ed9
WilberforceMalcolm <wilberforcemalcolm14@gmail.com>
2025-03-31 03:50:04 +0000

    nnmvm

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000; // Use Vercel’s port
+const port = process.env.PORT || 3000;
 
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
-app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
