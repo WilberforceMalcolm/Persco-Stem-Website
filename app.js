b0ce29ce767ee53ec8dd111e509b090be3c47cb3
WilberforceMalcolm <wilberforcemalcolm14@gmail.com>
2025-03-31 02:41:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 3000; // Use Vercel’s port
 
 app.set('view engine', 'ejs');
 app.set('views', '');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.listen(3000);
-
 
 app.get('/', (req, res)=> {
     res.render('index', {title: 'Home'})
@@ -26,4 +23,7 @@ app.get('/signin', (req, res)=> {
 app.get('/blog', (req, res)=> {
     res.render('blog', {title: 'Blogs'})
 })
+
+app.listen(port, () => {
+});
 
