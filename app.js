7de375d7d1e38606d7d3e6d40cc83a6c8926f265
WilberforceMalcolm <wilberforcemalcolm14@gmail.com>
2025-04-04 20:37:31 +0000

    mbnmj

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6000;
 
-const fs = require("fs");
+const mongoose = require("mongoose");
+
+const collection = require("./public/js/signup");
+
+
+const dbURI = "mongodb+srv://wilberforcemalcolm:Perscostem.@cluster0.wrbaifu.mongodb.net/Perscostem?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(dbURI)
+.then((results) => console.log("connected to db"))
+.catch((err) => console.log(err));
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+//convert data into json format 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 
 app.get('/', (req, res)=> {
@@ -34,7 +45,21 @@ app.get('/blog', (req, res)=> {
 })
 
 
+
+
+app.post("/signup", async (req, res) => {
+    const data = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password
+    }
+    const userdata = await collection.insertMany(data);
+    console.log(userdata);
+})
+
+
 app.listen(port, () => {
-    console.log("listening on port 3000")
+    console.log("listening on port 6000")
 });
 
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,84 +1,26 @@
+const { mongoose } = require("mongoose");
 
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+const Schema = mongoose.Schema;
 
-
-
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
-const firebaseConfig = {
-
-apiKey: "AIzaSyBYCL3-J-EaHPBKpfGh4r86q9ylAm7DyKA",
-authDomain: "perscostemloginsignup.firebaseapp.com",
-projectId: "perscostemloginsignup",
-storageBucket: "perscostemloginsignup.firebasestorage.app",
-messagingSenderId: "305349372287",
-appId: "1:305349372287:web:863ca78a6e5406225a124a",
-measurementId: "G-F2D2D4668K"
-
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-function showMessage(){
-    const messageDiv = document.getElementById("signUpMessage");
-    messageDiv.style.display = "block"; 
-    messageDiv.style.opacity = 1;
-    setTimeout(() => {
-        messageDiv.style.opacity = 0;
-    }, 5000)
-}
-
-
-const signUp = document.getElementById("submitSignUp");
-
-signUp.addEventListener("click", (event) => {
-    event.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const firstName = document.getElementById("Fname").value;
-    const lastName = document.getElementById("Lname").value;
-
-    const auth = getAuth();
-    const db = getFirestore();
-
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        const user = userCredential.user;
-        const userData = {
-            email: email,
-            firstName: firstName,
-            lastName: lastName  
-        }
-        showMessage();
-        const docRef = doc(db, "users", user.uid) 
-        setDoc(docRef, userData)
-        .then(()=> {
-            window.location.href = "./documentation.html";
-        }) 
-        .catch((error) => {
-            console.error("error writing document", error);
-        });
-
-    })
-    .catch((error) =>{
-        const errorCode = error.code;
-        if(errorCode  == "auth/email-already-in-use") {
-            showMessage();
-        }else {
-            showMessage();
-        }
-    })
+const detailsSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
 })
+
+const collection = new mongoose.model("users", detailsSchema);
+
+module.exports = collection;
