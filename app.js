f985dc8119fcfe729fab595cbe3cb286fffc30c2
WilberforceMalcolm <wilberforcemalcolm14@gmail.com>
2025-05-10 15:34:54 +0000

    fkkkf

--- app.js
+++ app.js
@@ -15,41 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-
-
-const pages = [
-    { url: '/index', changefreq: 'daily', priority: 1.0 },
-    { url: '/about', changefreq: 'monthly', priority: 0.7 },
-    { url: '/blog', changefreq: 'daily', priority: 0.8 },
-    { url: '/gallery', changefreq: 'weekly', priority: 0.8 },
-    { url: '/contact', changefreq: 'weekly', priority: 0.8 },
-  ];
-  
-
-  app.get('/sitemap.xml', (req, res) => {
-    res.header('Content-Type', 'application/xml');
-    
-    // Create the XML sitemap string
-    let sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n';
-    sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-  
-    // Loop through each page and generate the XML
-    pages.forEach(page => {
-      sitemap += `
-        <url>
-          <loc>https://perscostem-wilberforcemalcolms-projects.vercel.app${page.url}</loc>
-          <changefreq>${page.changefreq}</changefreq>
-          <priority>${page.priority}</priority>
-        </url>\n`;
-    });
-  
-    sitemap += '</urlset>';
-  
-    // Send the generated sitemap
-    res.send(sitemap);
-  });
-
-
 app.get('/', (req, res)=> {
     res.render('index', {title: 'Home'})
 })
